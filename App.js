57b1a572aaf404624fb936a435c02ce9bf3e7765
Likhith K.P <likhithkodi6789@gmail.com>
2022-10-25 08:39:04 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Home from './components/Home';
+import Books from './components/Books';
+
+function App() {
+  return (
+    <div className="App">
+      <Home/>
+      <Books/>
+    </div>
+  );
+}
+
+export default App;
